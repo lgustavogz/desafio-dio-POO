95b360eac21119d6e273a314e3cbb1f589f38095
Gustavo <luizgustavogz@hotmail.com>
2022-07-30 20:51:20 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.grassi.desafio.dominio.Bootcamp;
 import br.com.grassi.desafio.dominio.Curso;
+import br.com.grassi.desafio.dominio.Dev;
 import br.com.grassi.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,37 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /* System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria); */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devGustavo = new Dev();
+        devGustavo.setNome("Gustavo");
+        devGustavo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos:" + devGustavo.getConteudosInscritos());
+        devGustavo.progredir();
+        System.out.println("______");
+        System.out.println("Conteúdos inscritos:" + devGustavo.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos:" + devGustavo.getConteudosConcluidos());
+        System.out.println("XP:" + devGustavo.calcularTotalXp());
+
+        System.out.println("______");
+
+        Dev devFrancesca = new Dev();
+        devFrancesca.setNome("Francesca");
+        devFrancesca.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos:" + devFrancesca.getConteudosInscritos());
+        devFrancesca.progredir();
+        System.out.println("______");
+        System.out.println("Conteúdos inscritos:" + devFrancesca.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos:" + devFrancesca.getConteudosConcluidos());
+        System.out.println("XP:" + devFrancesca.calcularTotalXp());
     }
 }
